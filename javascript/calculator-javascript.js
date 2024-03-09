f7be907342a1b540dbdc353da3aec3db9611efb8
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-09 10:30:57 +0200

    fix: refactored listeners and display functions
    Refactored the 'toScreen' and 'toStatusScreen' fu-
    ctions to be universal rather than for a single u-
    se case.
    
    Added an 'operate' method which returns the answer
    of the user input into the calculator.

--- javascript/calculator-javascript.js
+++ javascript/calculator-javascript.js
@@ -8,8 +8,10 @@ const equals = document.querySelector('#equals');
 // Variables to store user input
 let num1;
 let num2;
+let operand;
 
 let onScreen = '';
+let onStatusScreen = '';
 
 // Basic Math operations
 const add = (a,b) => a + b;
@@ -17,47 +19,47 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
-const toScreen = (btn) => {
-    screen.textContent = `${onScreen}${btn.textContent}`;
+const toScreen = (string) => {
+    screen.textContent = `${onScreen}${string}`;
     onScreen = screen.textContent;
 }
 
-const toStatusScreen = (btn) => {
-    statusScreen.textContent = `${onScreen} ${btn.textContent}`;
-    console.log(statusScreen.textContent);
+const toStatusScreen = (string) => {
+    statusScreen.textContent = `${onStatusScreen}${string}`;
+    onStatusScreen = statusScreen.textContent;
+}
+
+const operate = (operand, num1, num2) => {
+    switch (operand) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+            return multiply(num1, num2);
+        case '÷':
+            return divide(num1, num2);
+    }
 }
 
 numpad.map((number) => {
     number.addEventListener('click', () => {
-        toScreen(number);
+        toScreen(number.textContent);
     });
 });
 
 operators.map((operator) => {
     operator.addEventListener('click', () => {
         num1 = Number(onScreen);
-        toStatusScreen(operator);
+        operand = operator.textContent;
+        toStatusScreen(`${onScreen} ${operand} `);
         screen.textContent = onScreen = '';
     });
 });
 
 equals.addEventListener('click', () => {
     num2 = Number(onScreen);
-    let operand = statusScreen.textContent.slice(-1);
-    statusScreen.textContent = `${statusScreen.textContent} ${onScreen} = `;
-    
-    switch (operand) {
-        case '+':
-            screen.textContent = add(num1, num2);
-            break;
-        case '-':
-            screen.textContent = subtract(num1, num2);
-            break;
-        case 'x':
-            screen.textContent = multiply(num1, num2);
-            break;
-        case '÷':
-            screen.textContent = divide(num1, num2);
-            break;
-    }
+    toStatusScreen(`${onScreen} =`)
+    onScreen = '';
+    toScreen(operate(operand, num1, num2)); 
 });
