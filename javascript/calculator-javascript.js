03685b8238e477b0e37d4b1417406abf116163c9
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-09 17:01:26 +0200

    feat: add logic for a single math expression

--- javascript/calculator-javascript.js
+++ javascript/calculator-javascript.js
@@ -8,19 +8,59 @@ const divide = (a,b) => a / b;
 let num1;
 let num2;
 let operator;
+let ans;
+
+let onScreen = '';
+let onSubScreen = '';
+
+// Query Selectos
+const numpad = [...document.querySelectorAll('.num')];
+const operators = [...document.querySelectorAll('.operator')];
+const equals = document.querySelector('#equals');
+const screen = document.querySelector('#screen');
+const subScreen = document.querySelector('#hist');
 
 function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
-            add(num1,num2);
-            break;
+            return add(num1,num2);
         case '-':
-            subtract(num1,num2);
-            break;
+            return subtract(num1,num2);
         case 'x':
-            multiply(num1,num2);
-            break;
+            return multiply(num1,num2);
         case '÷':
-            divide(num1,num2);
+            return divide(num1,num2);
     }
 }
+
+function setScreen(string) {
+    onScreen = screen.textContent = `${onScreen}${string}`;
+}
+
+function setSubScreen(string) {
+    onSubScreen = subScreen.textContent = `${onSubScreen}${string}`
+}
+
+numpad.map((number) => {
+    number.addEventListener('click', () => {
+        setScreen(number.textContent);
+    })
+})
+
+operators.map((operatr) => {
+    operatr.addEventListener('click', () => {
+        num1 = Number(onScreen);
+        operator = operatr.textContent;
+        setSubScreen(`${onScreen} ${operator} `);
+        onScreen = '';
+        setScreen('');
+    })
+})
+
+equals.addEventListener('click', () => {
+    num2 = Number(onScreen);
+    ans = operate(operator, num1, num2);
+    setSubScreen(`${num2} =`);
+    onScreen = '';
+    setScreen(`${ans}`);
+})
